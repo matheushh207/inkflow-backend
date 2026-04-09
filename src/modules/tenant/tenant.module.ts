266de556591e89673540b9c9704ea68692a2b1ed
Matheus InkFlow <matheus@inkflow.com>
2026-04-09 18:09:07 -0300

    Add PrismaModule import to TenantModule to fix dependency resolution

--- src/modules/tenant/tenant.module.ts
+++ src/modules/tenant/tenant.module.ts
@@ -1,9 +1,11 @@
 import { Module, Global } from '@nestjs/common';
 import { TenantService } from './tenant.service';
 import { TenantController } from './tenant.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Global()
 @Module({
+    imports: [PrismaModule],
     controllers: [TenantController],
     providers: [TenantService],
     exports: [TenantService],
